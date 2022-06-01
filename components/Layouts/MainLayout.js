7e126a659916f2028ddbfcbbb17f3e8046e831cf
tapia1013 <manbearpig1013@gmail.com>
2022-06-01 15:15:43 -0700

    creating header and adding background color to mainlayout

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -3,12 +3,15 @@ import Header from '../UI/Header'
 
 const MainLayout = (props) => {
   return (
-    <>
+    <div style={{
+      background: 'linear-gradient(135deg, rgba(2,27,64,1)11%,rgba(119,30,135,1)100%)',
+      minHeight: '100vh'
+    }}>
       <Header />
       <section className="content-container">
         {props.children}
       </section>
-    </>
+    </div >
   )
 }
 
