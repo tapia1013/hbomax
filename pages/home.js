de84b85b62010cb8915dc600c52a2048c8a3d616
tapia1013 <manbearpig1013@gmail.com>
2022-06-08 15:07:05 -0700

    just added component and styled done

--- pages/home.js
+++ pages/home.js
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import MainLayout from '../components/Layouts/MainLayout'
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
 import ForYouList from '../components/UI/ForYouList/ForYouList'
+import JustAdded from '../components/UI/JustAdded/JustAdded'
 
 export default function HomeView() {
   return (
     <MainLayout>
       <FeaturedMedia />
       <ForYouList />
+      <JustAdded />
     </MainLayout>
   )
 }
