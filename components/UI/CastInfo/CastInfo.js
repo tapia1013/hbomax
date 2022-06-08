e97b820a8ec2d725bb6dd0e04b6fe97fc888ec9a
tapia1013 <manbearpig1013@gmail.com>
2022-06-08 15:59:28 -0700

    done with movie view and cast and crew credits below it

--- components/UI/CastInfo/CastInfo.js
+++ components/UI/CastInfo/CastInfo.js
@@ -3,7 +3,62 @@ import React from 'react'
 const CastInfo = () => {
   return (
     <div className='cast-info'>
-      Cast Info
+      <div className='cast-info__group-title'>
+        Cast & Crew
+      </div>
+
+      <div className='cast-info__list'>
+        <ul className='cast-info__crew'>
+          <li>Director</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+
+        <ul className='cast-info__crew'>
+          <li>Nye</li>
+          <li>Riah Rod</li>
+        </ul>
+      </div>
+
+      <div className='cast-info__group-title'>
+        Director
+      </div>
+
+      <div className='cast-info__list'>
+        <ul className='cast-info__crew'>
+          <li>Director</li>
+          <li>Riah Rod</li>
+        </ul>
+      </div>
     </div>
   )
 }
